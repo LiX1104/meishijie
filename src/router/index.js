8b5e1c8d366c45c473cad92bc863f408280fd94c
王棋彬 <mrwangqibin@163.com>
2021-11-03 08:43:34 +0800

    提交了美食杰首页和购物车页

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+import {userInfo} from '@/service/api';
+
+import  Store  from '@/store'
+
+
+import Home from '@/views/home/Home.vue'
+const a=[
+    {
+        path:'/car',
+        name:"car",
+        component:()=> import ('@/views/car/car.vue')
+    },
+]
+const router = new Router({
+    mode:"history",
+    routes:[
+        {
+            path:'/',
+            name:"Home",
+            component:Home
+        },
+        ...a
+    ]
+});
+
+
+
+export default router;
