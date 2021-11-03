407a516fcb8b1cbcdab8aa5510d6465cc6009a23
MrsLi <LiX_1104@163.com>
2021-11-03 14:25:32 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -13,21 +13,17 @@ const a=[
         path:'/car',
         name:"car",
         component:()=> import ('@/views/car/car.vue')
-<<<<<<< HEAD
     },
     {
         path:'/detail',
         name:"detail",
         title:'详情',
         component:()=> import ('@/views/detail/detail.vue')
-    },
-=======
     },{
         path:'/login',
         name:"login",
         component:()=> import ('@/views/logon/index.vue')
     }
->>>>>>> b2066fd687b708ac84a05ac3dcaad9f31d8145c0
 ]
 const router = new Router({
     mode:"history",
